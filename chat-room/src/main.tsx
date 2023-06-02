7004bf0e265d4605b2ded01b9473752371c65e92
jeraruk <107743662+jeraruk@users.noreply.github.com>
2023-06-02 21:59:13 +1000

    Added vercel config for refresing on routes

--- chat-room/src/main.tsx
+++ chat-room/src/main.tsx
@@ -2,11 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
